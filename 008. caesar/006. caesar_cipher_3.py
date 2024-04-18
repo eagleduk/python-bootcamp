cd5bcb8dd9d80b05d1101a1d6b801c57ddbedaed
SJ.Lee <eagleduk@gmail.com>
2024-04-18 22:26:22 +0900

    008. caesar > caesar cipher step 3 solution

--- 008. caesar/006. caesar_cipher_3.py
+++ 008. caesar/006. caesar_cipher_3.py
@@ -5,26 +5,18 @@ text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
 #TODO-1: Combine the encrypt() and decrypt() functions into a single function called caesar().
-
-def encrypt(plain_text, shift_amount):
-  cipher_text = ""
-  for letter in plain_text:
+def caesar(caesar_text, shift_amount, direction):
+  result_text = ""
+  for letter in caesar_text:
     position = alphabet.index(letter)
     new_position = position + shift_amount
-    cipher_text += alphabet[new_position]
-  print(f"The encoded text is {cipher_text}")
 
-def decrypt(cipher_text, shift_amount):
-  plain_text = ""
-  for letter in cipher_text:
-    position = alphabet.index(letter)
-    new_position = position - shift_amount
-    plain_text += alphabet[new_position]
-  print(f"The decoded text is {plain_text}")
+    if direction == "decode":
+      position = alphabet.index(letter, 25)
+      new_position = position - shift_amount
 
-if direction == "encode":
-  encrypt(plain_text=text, shift_amount=shift)
-elif direction == "decode":
-  decrypt(cipher_text=text, shift_amount=shift)
+    result_text += alphabet[new_position]
+  print(f"The {"encode" if direction == "encode" else "decode"} text is {result_text}")
 
 #TODO-2: Call the caesar() function, passing over the 'text', 'shift' and 'direction' values.
+caesar(caesar_text=text, shift_amount=shift, direction=direction)
