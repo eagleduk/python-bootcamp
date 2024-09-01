487c8f50a01f2d55e4bb56d3a8961f2dc709ab99
SJ.Lee <eagleduk@gmail.com>
2024-09-01 22:16:11 +0900

    016. oop > coffee machine self solution

--- 016. oop/oop-coffee-machine-start/main.py
+++ 016. oop/oop-coffee-machine-start/main.py
@@ -1,3 +1,25 @@
 from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+
+coffee_machine = CoffeeMaker()
+coffee_menu = Menu()
+money = MoneyMachine()
+
+is_run = True
+
+while is_run:
+    answer = input("What would you like? (espresso/latte/cappuccino): ")
+
+    if answer == "off":
+        is_run = False
+        print("Have a nice day!")
+    elif answer == "report":
+        coffee_machine.report()
+        money.report()
+    else:
+        coffee_item = coffee_menu.find_drink(answer)
+        if (coffee_item is not None and
+                coffee_machine.is_resource_sufficient(coffee_item) and
+                money.make_payment(coffee_item.cost)):
+            coffee_machine.make_coffee(coffee_item)
