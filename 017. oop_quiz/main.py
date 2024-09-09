80b9d000acaf3a30f727a5d330d835eea6843ab6
SJ.Lee <eagleduk@gmail.com>
2024-09-09 21:58:04 +0900

    017. oop_quiz > quiz part 3

--- 017. oop_quiz/main.py
+++ 017. oop_quiz/main.py
@@ -1,5 +1,6 @@
 from data import question_data
 from question_model import Question
+from quiz_brain import QuizBrain
 
 question_bank = []
 
@@ -7,3 +8,6 @@ for d in question_data:
     question_bank.append(Question(d["text"], d["answer"]))
 
 print(question_bank)
+
+quiz = QuizBrain(question_bank)
+quiz.next_question()
