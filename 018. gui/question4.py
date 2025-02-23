e48446f2188b66b89e93259e2f1ab18f5c3d1fe8
SJ.Lee <eagleduk@gmail.com>
2025-02-23 23:08:10 +0900

    018. gui > Question 4. random generate color rgb

--- 018. gui/question4.py
+++ 018. gui/question4.py
@@ -1,20 +1,29 @@
+import turtle
 from turtle import Turtle, Screen
 import random
+
+turtle.colormode(255)
+
+
+def random_color():
+    r = random.randint(0,255)
+    g = random.randint(0,255)
+    b = random.randint(0,255)
+
+    return (r, g, b)
+
 
 the_turtle = Turtle()
 the_turtle.shape("turtle")
 the_turtle.pensize(10)
+the_turtle.speed("fastest")
 
-colors = ["green yellow", "magenta", "dark green", "blue", "maroon", "black", "dark cyan", "royal blue", "saddle brown", "gold", "medium violet red", "hot pink"]
-
-for _ in range(15):
-    idx = random.randrange(1,5)
-    the_turtle.color(random.choice(colors))
+for _ in range(200):
+    idx = random.randrange(0,4)
+    color = random_color()
+    the_turtle.color(color)
     angle = 90 * idx
-    print(angle)
     the_turtle.right(angle)
     the_turtle.forward(30)
-
-
 
 Screen().exitonclick()
