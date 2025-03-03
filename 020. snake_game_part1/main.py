a34a4eefed22006261b6abc8a7b867aa59ecf6f0
SJ.Lee <eagleduk@gmail.com>
2025-03-03 22:37:16 +0900

    020. snake_game_part1 > snake move and turn

--- 020. snake_game_part1/main.py
+++ 020. snake_game_part1/main.py
@@ -2,11 +2,24 @@ from turtle import Screen, Turtle
 import time
 import snake
 
+def up():
+    print("up")
+
+def down():
+    print("down")
+
+def left():
+    print("left")
+
+def right():
+    print("right")
+
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("My Snake Game")
 screen.tracer(0)
+screen.listen()
 
 body_length = 3
 
@@ -25,9 +38,23 @@ while turn < 100:
     screen.update()
     turn = turn + 1
 
-    for segment in segments:
-        segment.forward()
+    forward_position = None
+    positions = []
 
+    for segment in segments:
+
+        t_position = segment.getposition()
+
+        if forward_position is None:
+            segment.forward()
+            screen.onkey(key="Up", fun=segment.up)
+            screen.onkey(key="Down", fun=segment.down)
+            screen.onkey(key="Left", fun=segment.left)
+            screen.onkey(key="Right", fun=segment.right)
+        else:
+            segment.setposition(forward_position[0], forward_position[1])
+
+        forward_position = t_position
 
 
 
--- 020. snake_game_part1/snake.py
+++ 020. snake_game_part1/snake.py
@@ -18,3 +18,19 @@ class SnakeSegment:
 
     def getposition(self):
         return (self._segment.xcor(), self._segment.ycor())
+
+    def up(self):
+        if self._segment.heading() != 270:
+            self._segment.setheading(90)
+
+    def down(self):
+        if self._segment.heading() != 90:
+            self._segment.setheading(270)
+
+    def left(self):
+        if self._segment.heading() != 0:
+            self._segment.setheading(180)
+
+    def right(self):
+        if self._segment.heading() != 180:
+            self._segment.setheading(0)
