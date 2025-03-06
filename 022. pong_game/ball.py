098346c1c857312e43ce02ac0939ec042755ee18
SJ.Lee <eagleduk@gmail.com>
2025-03-06 21:59:07 +0900

    022. pong_game > ball bounce wall

--- 022. pong_game/ball.py
+++ 022. pong_game/ball.py
@@ -2,13 +2,23 @@ from turtle import Turtle
 
 class Ball(Turtle):
 
+    _x = 10
+    _y = -10
+
     def __init__(self):
         super().__init__()
         self.penup()
         self.shape("circle")
         self.color("white")
-        self.setheading(45)
         self.speed(0)
 
     def run(self):
-        self.forward(10)
+        x = self.xcor() + self._x
+        y = self.ycor() + self._y
+
+        if 280 <= y:
+            self._y = -10
+        elif y <= -280:
+            self._y = 10
+
+        self.goto(x, y)
