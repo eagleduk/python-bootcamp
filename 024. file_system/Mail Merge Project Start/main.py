c49bb2e596890b38e909e7e950eab93548fe2ffc
SJ.Lee <eagleduk@gmail.com>
2025-03-12 21:42:15 +0900

    024. file_system > mail merge project

--- 024. file_system/Mail Merge Project Start/main.py
+++ 024. file_system/Mail Merge Project Start/main.py
@@ -6,3 +6,14 @@
 #Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
     #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
         #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
+
+with open("./input/Letters/starting_letter.txt") as s:
+        letter_template = s.read()
+
+
+with open("./input/Names/invited_names.txt") as n:
+    names = n.read().split("\n")
+
+for name in names:
+    with open(f"./Output/ReadyToSend/letter_to_{name}", mode="w") as l:
+        l.write(letter_template.replace("[name]", name))
