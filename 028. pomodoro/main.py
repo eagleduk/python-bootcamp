7faa376f617ab9c911bf40d85f54d16ea0a2a9e3
SJ.Lee <eagleduk@gmail.com>
2025-03-23 22:55:30 +0900

    028. pomodoro > count down start event

--- 028. pomodoro/main.py
+++ 028. pomodoro/main.py
@@ -9,24 +9,47 @@ FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
+repos = 0
 
 # ---------------------------- TIMER RESET ------------------------------- #
 def count_reset():
     canvas.itemconfig(timer_text, text="00:00")
 
-
 # ---------------------------- TIMER MECHANISM ------------------------------- #
 def start_event():
-    count_down(5 * 60)
+    global repos
+    work_sec = WORK_MIN * 1
+    short_break_sec = SHORT_BREAK_MIN * 1
+    long_break_sec = LONG_BREAK_MIN * 1
+
+    if repos == 0 or repos == 2 or repos == 4 or repos == 6:
+        timer_label["text"]="Work"
+        timer_label["fg"]=GREEN
+        count_down(work_sec)
+    elif repos == 1 or repos == 3 or repos == 5:
+        timer_label["text"]="Break"
+        timer_label["fg"]=PINK
+        count_down(short_break_sec)
+    elif repos == 7:
+        timer_label["text"]="Break"
+        timer_label["fg"]=RED
+        count_down(long_break_sec)
+
+    repos += 1
+
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
 def count_down(count):
-
     count_min = math.floor(count / 60)
     count_sec = count % 60
 
     canvas.itemconfig(timer_text, text=f"{str(count_min).zfill(2)}:{str(count_sec).zfill(2)}")
     if count > 0:
         window.after(1000, count_down, count - 1)
+    else:
+        start_event()
+        if repos == 0 or repos == 2 or repos == 4 or repos == 6:
+            check_label["text"] += "✔"
+
 # ---------------------------- UI SETUP ------------------------------- #
 
 window = Tk()
@@ -48,7 +71,7 @@ start_button.grid(row=2, column=0)
 reset_button = Button(text="Reset", command=count_reset)
 reset_button.grid(row=2, column=2)
 
-check_label = Label(text="✔", fg=GREEN, bg=YELLOW)
+check_label = Label(fg=GREEN, bg=YELLOW)
 check_label.grid(row=3, column=1)
 
 window.mainloop()
