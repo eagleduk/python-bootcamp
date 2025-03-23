916921ae452e5083db3c8a334485d9a6ea4cc184
SJ.Lee <eagleduk@gmail.com>
2025-03-23 21:54:31 +0900

    028. pomodoro > pomodoro layouts

--- 028. pomodoro/main.py
+++ 028. pomodoro/main.py
@@ -25,7 +25,18 @@ canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="./tomato.png")
 canvas.create_image(100, 112, image=tomato_img)
 canvas.create_text(100, 130, text="00:00", font=(FONT_NAME, 35, "bold"), fill="white")
-canvas.pack()
+canvas.grid(row=1, column=1)
 
+timer_label = Label(text="Timer", fg=GREEN, font=(FONT_NAME, 38, "bold"), bg=YELLOW)
+timer_label.grid(row=0, column=1)
+
+start_button = Button(text="Start")
+start_button.grid(row=2, column=0)
+
+reset_button = Button(text="Reset")
+reset_button.grid(row=2, column=2)
+
+check_label = Label(text="✔", fg=GREEN, bg=YELLOW)
+check_label.grid(row=3, column=1)
 
 window.mainloop()
