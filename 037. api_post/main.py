1c25ed84e003a695bd898e2eda752dbab083a948
SJ.Lee <eagleduk@gmail.com>
2025-04-16 22:12:52 +0900

    037. api_post > post pixel to graph

--- 037. api_post/main.py
+++ 037. api_post/main.py
@@ -30,5 +30,13 @@ create_graph_body = {
     "color": "momiji",
 }
 
-create_graph_response = requests.post(url=create_graph_endpoint, json=create_graph_body, headers=TOKENHEADERS)
-print("Create Graph Response: " + create_graph_response.text)
+# create_graph_response = requests.post(url=create_graph_endpoint, json=create_graph_body, headers=TOKENHEADERS)
+# print("Create Graph Response: " + create_graph_response.text)
+
+post_graph_pixel_endpoint = f"{create_graph_endpoint}/{GRAPHID}"
+post_graph_pixel_body = {
+    "date": "20250416",
+    "quantity": "2.3"
+}
+post_graph_pixel_response = requests.post(url=post_graph_pixel_endpoint, json=post_graph_pixel_body, headers=TOKENHEADERS)
+print("Post Graph Pixel Response: " + post_graph_pixel_response.text)
