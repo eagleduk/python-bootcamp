ade58249fe66741b1df0b77a01f23ecf9d24cc52
SJ.Lee <eagleduk@gmail.com>
2025-06-15 23:23:34 +0900

    045. beatifulsoup > live scraping and write

--- 045. beautifulsoup/main.py
+++ 045. beautifulsoup/main.py
@@ -1,32 +1,16 @@
 from bs4 import BeautifulSoup
 import requests
 
-response = requests.get("https://appbrewery.github.io/news.ycombinator.com/")
+URL = "https://www.empireonline.com/movies/features/best-movies-2/"
 
-html_content = response.text
+response = requests.get(URL)
 
-soup = BeautifulSoup(html_content, "html.parser")
+soup = BeautifulSoup(response.text, "html.parser")
 
-all_vote_tr_tags = soup.find_all(name="tr", class_="athing")
+titles = soup.select(selector=".content_content__i0P3p h2 strong")
 
-all_ranks = []
+ranks = [strong.getText() for strong in titles]
 
-for vote_tr_tag in all_vote_tr_tags:
-    anchor = vote_tr_tag.find(name="a", class_="storylink")
-    text = anchor.getText()
-    href = anchor.get("href")
-    id = vote_tr_tag.get("id")
-    score_span = vote_tr_tag.find_next("tr").find(name="span", class_="score")
-    score = score_span.getText().split(" ")[0]
-    print(f"text: {text}, link: {href}, score={score}")
-
-    all_ranks.append({
-        "text": text,
-        "link": href,
-        "score": int(score)
-    })
-
-print(all_ranks)
-
-sort_ranks = sorted(all_ranks, key=lambda x: x["score"], reverse=True)
-print(sort_ranks)
+with open("moves.txt", "+w", encoding="UTF-8") as file:
+    for rank in reversed(ranks):
+        file.write(rank + "\n")
