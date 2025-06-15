84c1930c4280f8d70bbfc3a77e784ea4cadc1153
SJ.Lee <eagleduk@gmail.com>
2025-06-15 22:39:06 +0900

    045. beatifulsoup > find elements

--- 045. beautifulsoup/README.md
+++ 045. beautifulsoup/README.md
@@ -11,3 +11,28 @@ with open("xxx.html") as file:
 soup = BeautifulSoup(contents, "html.parser")
 print(soup)
 ```
+
+### 2. find Tags
+
+1. tag로 찾기
+    ```python
+    print(soup.p)
+    print(soup.a)
+    ```
+   - 찾은 tag의 속성 및 컨텐츠 찾기
+    ```python
+    print(soup.p.get("class"))
+    print(soup.p.text())
+    ```
+   
+2. 일치하는 element 찾기
+```python
+soup.find(name="")
+soup.find_all(name="")
+```
+
+3. css selector 정보로 찾기
+```python
+soup.select_one(selector="")
+soup.select(selector="")
+```
--- 045. beautifulsoup/main.py
+++ 045. beautifulsoup/main.py
@@ -6,3 +6,20 @@ with open("./website.html", encoding="UTF-8") as file:
 soup = BeautifulSoup(contents, "html.parser")
 
 print(soup.prettify())
+
+print(soup.a)
+print(soup.a.get("href"))
+print(soup.a.getText())
+
+first_anchor_tag = soup.find(name="a")
+print(first_anchor_tag)
+
+all_anchor_tags = soup.find_all(name="a")
+print(all_anchor_tags)
+for tag in all_anchor_tags:
+    print(tag.getText())
+
+all_selector_tags = soup.select(selector="a")
+print(all_selector_tags)
+
+print(soup.select_one(selector="a"))
